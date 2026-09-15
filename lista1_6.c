8fda72017952202ae1a79817c858bc4fccc3aaf4
Lucas Dias <lucasmachadias@gmail.com>
2026-09-15 17:45:24 -0300

    Update lista1_6.c

--- lista1_6.c
+++ lista1_6.c
@@ -7,18 +7,17 @@ Restrição: Dentro da função, percorra o vetor utilizando ponteiros, sem util
 
 #include <stdio.h>
 
-// Retorna a soma dos elementos do vetor.
 int soma(int *v, int n){
     int acumulador = 0;
     for(int i = 0; i < n; i++){
-        acumulador = acumulador + v[i]; // Soma cada elemento do vetor.
+        acumulador = acumulador + v[i];
     }
-    return acumulador; // Retorna a soma dos elementos do vetor.
+    return acumulador;
 }
 
 int main(){
-    int v[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Cria e inicializa um vetor com dez elementos.
-    int n = 10; // Define o tamanho do vetor.
-    printf("A soma dos elementos do vetor e: %d\n", soma(v, n)); // Chama a função soma passando o vetor e seu tamanho.
-    return 0; // Indica que o programa terminou corretamente.
+    int v[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = 10;
+    printf("A soma dos elementos do vetor e: %d\n", soma(v, n));
+    return 0;
 }

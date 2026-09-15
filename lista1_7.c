48b15eaf8d20bf367603ee0d5b25cb24e4b541d9
Lucas Dias <lucasmachadias@gmail.com>
2026-09-15 17:46:02 -0300

    Update lista1_7.c

--- lista1_7.c
+++ lista1_7.c
@@ -13,19 +13,19 @@ Exemplo de execução:
 
 void troca_vizinhos(int *a, int *b){
     int temp;
-    temp = *a; // Armazena o valor do primeiro elemento em uma variável temporária.
-    *a = *b; // Atribui o valor do segundo elemento ao primeiro.
-    *b = temp; // Atribui o valor armazenado na variável temporária ao segundo elemento.
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int main(){
-    int v[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}; // Cria e inicializa um vetor com dez elementos.
-    int n = 10; // Define o tamanho do vetor.
-    for(int i = 0; i < n; i = i + 2){ // Percorre o vetor de dois em dois elementos.
+    int v[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int n = 10;
+    for(int i = 0; i < n; i = i + 2){
         troca_vizinhos(&v[i], &v[i + 1]);
     }
     for(int i = 0; i < n; i++){
-        printf("%d ", v[i]); // Imprime os elementos do vetor após a troca.
+        printf("%d ", v[i]);
     }
     return 0;
 }
